21aa580a762cf2a3be8e32eaf526f28bbc44b990
Daniel Wilson <danielvillenawilson@gmail.com>
2023-03-15 17:49:25 +0100

    change(create login)

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Login = () => {
+  return (
+    <div className="bg-[url('../../assets/images/fondo.png')]">
+        <h1>Holiiiii</h1>
+    </div>
+  )
+}
+
+export default Login
