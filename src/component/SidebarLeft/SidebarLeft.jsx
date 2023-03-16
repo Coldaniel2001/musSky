3ce7e10b4231e9a8e5be6bb5474798b287c3952e
Victor Jauregui <vicjaudom@hotmail.com>
2023-03-16 15:22:10 +0100

    starting sidebar

--- src/component/SidebarLeft/SidebarLeft.jsx
+++ src/component/SidebarLeft/SidebarLeft.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import logo from '../../assets/images/logo.png'
+import iconHome from '../../assets/images/icon-home.png'
 
 const SidebarLeft = () => {
-	return <div>SidebarLeft</div>;
+	return (
+		
+		<div className='text-white flex flex-col items-center'>
+			<img className='w-60 mt-6' src={logo} alt="logo" />
+			<div>
+				<div className='grid grid-cols-5 items-center ml-6'>
+					<img className='w-12 grid span-cols-4' src={iconHome} alt="home icon" />
+					<p className='text-[1.2rem] font-bold'>Home</p>
+				</div>
+			</div>
+		</div>
+
+		
+	)
+	
 };
 
 export default SidebarLeft;
--- src/page/HomePage/HomePage.jsx
+++ src/page/HomePage/HomePage.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import SidebarLeft from '../../component/SidebarLeft/SidebarLeft'
 
 const HomePage = () => {
   return (
-    <div>Home</div>
+    <div className='bg-gradient-to-tr from-black via-black to-[#7339E5] h-screen flex'>
+      <div className='w-[20%] border border-r-white'>
+        <SidebarLeft />
+      </div>
+      <div className='w-[65%] border border-r-white'>
+
+      </div>
+      <div className='w-[15%] border border-r-white'>
+
+      </div>
+    </div>
+    
   )
 }
 
