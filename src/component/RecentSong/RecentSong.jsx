1d0ef2ce053dc42a4b33e17257045ee82d078391
Victor Jauregui <vicjaudom@hotmail.com>
2023-03-21 10:44:53 +0100

    change(the responsive Dashboard is finish)

--- src/component/RecentSong/RecentSong.jsx
+++ src/component/RecentSong/RecentSong.jsx
@@ -6,93 +6,93 @@ import more from '../../assets/images/icons/more.png'
 const RecentSong = () => {
   return (
     
-        <div className='flex overflow-x-scroll w-full whitespace-no-wrap 2xl:grid 2xl:grid-cols-2'>
-            <div className='min-w-[25rem] lg:min-w-[35rem] flex justify-center mb-4 '>
+        <div className='flex overflow-x-scroll scrollbar-hide w-full whitespace-no-wrap 2xl:grid 2xl:grid-cols-2'>
+            <div className='min-w-[25rem] flex justify-center mb-4 '>
                 <div className=' bg-[#7239e546] grid grid-cols-8 w-[90%] rounded'>
-                    <img className='h-full rounded-l opacity-90' src={antonioJose} alt="first artist" />
-                    <div className='col-span-5 ml-5 grid items-center'>
+                    <img className='col-span-2 h-[5rem] rounded-l opacity-90' src={antonioJose} alt="first artist" />
+                    <div className='col-span-4  grid items-center'>
                         <p className='text-[1.5rem] font-bold'>Antonio José</p>
                         <p className='font-thin'>Wilson estoy enamorado de ti</p>
                     </div>
-                    <img className='my-auto w-[80%] mx-auto' src={bluePlay} alt="play music" />
-                    <img className='w-[40%] mx-auto my-auto' src={more} alt="" />
+                    <img className='hidden 2xl:grid my-auto w-[80%] mx-auto' src={bluePlay} alt="play music" />
+                    <img className='col-span-2 2xl:col-span-1 justify-end w-[25%] 2xl:w-[40%] mx-auto my-auto' src={more} alt="" />
                 </div>
             </div>
             <div className='min-w-[25rem] flex justify-center mb-4 '>
                 <div className=' bg-[#7239e546] grid grid-cols-8 w-[90%] rounded'>
-                    <img className='h-full rounded-l opacity-90' src={antonioJose} alt="first artist" />
-                    <div className='col-span-5 ml-5 grid items-center'>
+                    <img className='col-span-2 h-[5rem] rounded-l opacity-90' src={antonioJose} alt="first artist" />
+                    <div className='col-span-4  grid items-center'>
                         <p className='text-[1.5rem] font-bold'>Antonio José</p>
                         <p className='font-thin'>Wilson estoy enamorado de ti</p>
                     </div>
-                    <img className='my-auto w-[80%] mx-auto' src={bluePlay} alt="play music" />
-                    <img className='w-[40%] mx-auto my-auto' src={more} alt="" />
+                    <img className='hidden 2xl:grid my-auto w-[80%] mx-auto' src={bluePlay} alt="play music" />
+                    <img className='col-span-2 2xl:col-span-1 justify-end w-[25%] 2xl:w-[40%] mx-auto my-auto' src={more} alt="" />
                 </div>
             </div>
             <div className='min-w-[25rem] flex justify-center mb-4 '>
                 <div className=' bg-[#7239e546] grid grid-cols-8 w-[90%] rounded'>
-                    <img className='h-full rounded-l opacity-90' src={antonioJose} alt="first artist" />
-                    <div className='col-span-5 ml-5 grid items-center'>
+                    <img className='col-span-2 h-[5rem] rounded-l opacity-90' src={antonioJose} alt="first artist" />
+                    <div className='col-span-4  grid items-center'>
                         <p className='text-[1.5rem] font-bold'>Antonio José</p>
                         <p className='font-thin'>Wilson estoy enamorado de ti</p>
                     </div>
-                    <img className='my-auto w-[80%] mx-auto' src={bluePlay} alt="play music" />
-                    <img className='w-[40%] mx-auto my-auto' src={more} alt="" />
+                    <img className='hidden 2xl:grid my-auto w-[80%] mx-auto' src={bluePlay} alt="play music" />
+                    <img className='col-span-2 2xl:col-span-1 justify-end w-[25%] 2xl:w-[40%] mx-auto my-auto' src={more} alt="" />
                 </div>
             </div>
             <div className='min-w-[25rem] flex justify-center mb-4 '>
                 <div className=' bg-[#7239e546] grid grid-cols-8 w-[90%] rounded'>
-                    <img className='h-full rounded-l opacity-90' src={antonioJose} alt="first artist" />
-                    <div className='col-span-5 ml-5 grid items-center'>
+                    <img className='col-span-2 h-[5rem] rounded-l opacity-90' src={antonioJose} alt="first artist" />
+                    <div className='col-span-4  grid items-center'>
                         <p className='text-[1.5rem] font-bold'>Antonio José</p>
                         <p className='font-thin'>Wilson estoy enamorado de ti</p>
                     </div>
-                    <img className='my-auto w-[80%] mx-auto' src={bluePlay} alt="play music" />
-                    <img className='w-[40%] mx-auto my-auto' src={more} alt="" />
+                    <img className='hidden 2xl:grid my-auto w-[80%] mx-auto' src={bluePlay} alt="play music" />
+                    <img className='col-span-2 2xl:col-span-1 justify-end w-[25%] 2xl:w-[40%] mx-auto my-auto' src={more} alt="" />
                 </div>
             </div>
             <div className='min-w-[25rem] flex justify-center mb-4 '>
                 <div className=' bg-[#7239e546] grid grid-cols-8 w-[90%] rounded'>
-                    <img className='h-full rounded-l opacity-90' src={antonioJose} alt="first artist" />
-                    <div className='col-span-5 ml-5 grid items-center'>
+                    <img className='col-span-2 h-[5rem] rounded-l opacity-90' src={antonioJose} alt="first artist" />
+                    <div className='col-span-4  grid items-center'>
                         <p className='text-[1.5rem] font-bold'>Antonio José</p>
                         <p className='font-thin'>Wilson estoy enamorado de ti</p>
                     </div>
-                    <img className='my-auto w-[80%] mx-auto' src={bluePlay} alt="play music" />
-                    <img className='w-[40%] mx-auto my-auto' src={more} alt="" />
+                    <img className='hidden 2xl:grid my-auto w-[80%] mx-auto' src={bluePlay} alt="play music" />
+                    <img className='col-span-2 2xl:col-span-1 justify-end w-[25%] 2xl:w-[40%] mx-auto my-auto' src={more} alt="" />
                 </div>
             </div>
             <div className='min-w-[25rem] flex justify-center mb-4 '>
                 <div className=' bg-[#7239e546] grid grid-cols-8 w-[90%] rounded'>
-                    <img className='h-full rounded-l opacity-90' src={antonioJose} alt="first artist" />
-                    <div className='col-span-5 ml-5 grid items-center'>
+                    <img className='col-span-2 h-[5rem] rounded-l opacity-90' src={antonioJose} alt="first artist" />
+                    <div className='col-span-4  grid items-center'>
                         <p className='text-[1.5rem] font-bold'>Antonio José</p>
                         <p className='font-thin'>Wilson estoy enamorado de ti</p>
                     </div>
-                    <img className='my-auto w-[80%] mx-auto' src={bluePlay} alt="play music" />
-                    <img className='w-[40%] mx-auto my-auto' src={more} alt="" />
+                    <img className='hidden 2xl:grid my-auto w-[80%] mx-auto' src={bluePlay} alt="play music" />
+                    <img className='col-span-2 2xl:col-span-1 justify-end w-[25%] 2xl:w-[40%] mx-auto my-auto' src={more} alt="" />
                 </div>
             </div>
             <div className='min-w-[25rem] flex justify-center mb-4 '>
                 <div className=' bg-[#7239e546] grid grid-cols-8 w-[90%] rounded'>
-                    <img className='h-full rounded-l opacity-90' src={antonioJose} alt="first artist" />
-                    <div className='col-span-5 ml-5 grid items-center'>
+                    <img className='col-span-2 h-[5rem] rounded-l opacity-90' src={antonioJose} alt="first artist" />
+                    <div className='col-span-4  grid items-center'>
                         <p className='text-[1.5rem] font-bold'>Antonio José</p>
                         <p className='font-thin'>Wilson estoy enamorado de ti</p>
                     </div>
-                    <img className='my-auto w-[80%] mx-auto' src={bluePlay} alt="play music" />
-                    <img className='w-[40%] mx-auto my-auto' src={more} alt="" />
+                    <img className='hidden 2xl:grid my-auto w-[80%] mx-auto' src={bluePlay} alt="play music" />
+                    <img className='col-span-2 2xl:col-span-1 justify-end w-[25%] 2xl:w-[40%] mx-auto my-auto' src={more} alt="" />
                 </div>
             </div>
             <div className='min-w-[25rem] flex justify-center mb-4 '>
                 <div className=' bg-[#7239e546] grid grid-cols-8 w-[90%] rounded'>
-                    <img className='h-full rounded-l opacity-90' src={antonioJose} alt="first artist" />
-                    <div className='col-span-5 ml-5 grid items-center'>
+                    <img className='col-span-2 h-[5rem] rounded-l opacity-90' src={antonioJose} alt="first artist" />
+                    <div className='col-span-4  grid items-center'>
                         <p className='text-[1.5rem] font-bold'>Antonio José</p>
                         <p className='font-thin'>Wilson estoy enamorado de ti</p>
                     </div>
-                    <img className='my-auto w-[80%] mx-auto' src={bluePlay} alt="play music" />
-                    <img className='w-[40%] mx-auto my-auto' src={more} alt="" />
+                    <img className='hidden 2xl:grid my-auto w-[80%] mx-auto' src={bluePlay} alt="play music" />
+                    <img className='col-span-2 2xl:col-span-1 justify-end w-[25%] 2xl:w-[40%] mx-auto my-auto' src={more} alt="" />
                 </div>
             </div>
             
