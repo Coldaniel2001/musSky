1ca984e197ab3da03af485f44ca6f9f057847477
Victor Jauregui <vicjaudom@hotmail.com>
2023-03-24 18:28:56 +0100

    design music played done

--- src/component/MusicPlayed/MusicPlayed.jsx
+++ src/component/MusicPlayed/MusicPlayed.jsx
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import AudioPlayer from 'react-h5-audio-player';
+import './musicPlayed.css'
+import play from '../../assets/images/icons/play.png'
+import next from '../../assets/images/icons/next.png'
+import previous from '../../assets/images/icons/previous.png'
+import pause from '../../assets/images/icons/pause.png'
 
 // import MusicSong from '../MusicSong/MusicSong';
 
@@ -27,44 +32,26 @@ const MusicPlayed = () => {
     <>
       <div className='md:hidden lg:block w-3/4 mx-auto text-red'>
         <img className='w-full mx-auto mt-10 mb-4 rounded-xl' src={dataSong.thumbnail} alt="played song" />
-        <p className='font-bold lg:text-[1.2rem] text-[2rem] w-[80%]'>{dataSong.artist}</p>
-        <p className='text-gray-500'>{dataSong.name}</p>
-        {/* <div className='grid grid-cols-4 mt-5 items-center  w-full'>
- 				<img className='w-12' src={previous} alt="previous song" />
- 				<img className='lg:w-12 xl:w-20 col-span-2 mx-auto' src={play} alt="play song" />
- 				<div className='flex justify-end '>
- 					<img className='w-12' src={next} alt="next song" />
-         				</div>
-         			</div> */}
+        <p className='font-bold lg:text-[1.2rem] text-[2rem] text-white w-[80%]'>{dataSong.artist}</p>
+        <p className='text-gray-500 '>{dataSong.name}</p>
       </div>
-      <div className='flex justify-center w-[20rem]'>
-        <AudioPlayer
+      <div className='mx-auto w-[20rem]'>
+        <AudioPlayer className='w-full flex justify-center'
           autoPlay={false}
+          customIcons={{
+            play: <img className="my-play-icon" src={play} alt="play"></img>,
+            pause: <img className="my-pause-icon" src={pause} alt="play"></img>,
+            next: <img className="my-next-icon" src={next} alt="play"></img>,
+            previous: <img className="my-previous-icon" src={previous} alt="play"></img>,
+            forward: <img className="my-forward-icon" src={previous} alt="play"></img>,
+            rewind: <img className="my-forward-icon" src={previous} alt="play"></img>,
+          }}
           src={dataSong.url}
           showSkipControls={true}
           onClickNext={e => setCurrentSong(currentSong + 1)}
-          onClickPrevious={e => currentSong>1?setCurrentSong(currentSong - 1):null}
+          onClickPrevious={e => currentSong>1? setCurrentSong(currentSong - 1):null}
         />
       </div>
-
-
-
-
-
-
-
-      {/* {dataSong.map((song)=>{
-					return (
-            <>
-            <MusicSong
-            key={song.id}
-            song={song}
-            />
-         </>
-					)
-				})} */}
-
-
     </>
   )
 
