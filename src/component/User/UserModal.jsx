7ef2ba2197b377b8f9dc56dad90943ad4191ab2f
Giorgi Meshveliani <xgorgaslax@gmail.com>
2023-03-27 10:43:07 +0200

    .

--- src/component/User/UserModal.jsx
+++ src/component/User/UserModal.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function UserModal() {
 
    return (
-    <div className='sm:w-[13rem] h-[6rem] bg-[#f4f1f7] absolute hover:opacity-100 ml-[-2rem] rounded-md   modal'>
+    <div className='sm:w-[13rem] h-[6rem] bg-[#f4f1f7] absolute hover:opacity-100 ml-[-2rem] rounded-md'>
     <p className=' hover:bg-[#7239e526] ml-2 cursor-pointer '>Mi cuenta</p>
     <p className=' hover:bg-[#7239e526] ml-2 cursor-pointer '>Ajustes</p>
    <Link to={"/login"}><p className=' ml-2  hover:bg-[#7239e526] '>Salir</p></Link> 
