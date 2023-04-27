3ee8d15979a3a6c46ec648e399a40e317624a13c
Alvaro Sanchez <alvarosanro89@gmail.com>
2023-04-27 08:59:01 +0200

    change size of components

--- src/component/SidebarRight/Top10/Top10.jsx
+++ src/component/SidebarRight/Top10/Top10.jsx
@@ -19,7 +19,7 @@ const Top10 = () => {
     
     <div className='flex items-center'>
       <div className='bg-[#2F2F2F] my-auto rounded-xl mr-2 mt-10'>
-        <div className=' bg-black w-[94%] mx-auto pb-10 mb-20' >
+        <div className=' bg-black w-[94%] mx-auto pb-10 mb-20 lg:h-[65vh] lg:overflow-y-scroll' >
           <p className='text-white font-bold text-3xl pt-5 ml-2'>TOP 10</p>
           <Top10SingularSong dataSong={dataSong} />
         </div>
--- src/page/HomePage/HomePage.jsx
+++ src/page/HomePage/HomePage.jsx
@@ -8,18 +8,18 @@ import TopPhone from '../../component/TopPhone/TopPhone'
 
 const HomePage = () => {
   return (
-    <div className='bg-gradient-to-tr from-black via-black to-[#7339E5] lg:h-screen  lg:flex'>
+    <div className='bg-gradient-to-tr from-black via-black to-[#7339E5] lg:min-h-screen lg:flex'>
       <div className=''>
         <TopPhone />
         <Top10Phone />
       </div>
-      <div className='hidden lg:block w-full lg:w-[15%] xl:w-[25%] 2xl:w-[18%] pt-10 border-r-2 border-gray-700'>
+      <div className='hidden lg:block w-full lg:w-[15%] xl:w-[25%] 2xl:w-[20%] pt-10 border-r-2 border-gray-700'>
         <SidebarLeft />
       </div>
-      <div className='w-full pt-10 lg:pt-0 lg:w-[85%] xl:w-[75%] 2xl:w-[67%]'>
+      <div className='w-full pt-10 lg:pt-0 lg:w-[85%] xl:w-[75%] 2xl:w-[62%]'>
         <CenterSection />
       </div>
-      <div className='hidden w-full lg:hidden xl:hidden 2xl:block 2xl:w-[15%]'>
+      <div className='hidden w-full lg:hidden xl:hidden 2xl:block 2xl:w-[18%]'>
         <SidebarRight />
       </div>
       <div className='lg:hidden'>
