e86daa35febd833c18bcb3ef5aac5b261bd2fa96
Giorgi Meshveliani <xgorgaslax@gmail.com>
2023-05-10 17:33:46 +0200

    .

---
+++ src/Detail Modal/DetailModal.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function DetailModal() {
+  return (
+    <div className='overflow-visible flex jusify-item-center absolute top-[3.5rem] right-[2rem] z-50 '>
+    <div className='w-[11rem] h-[4rem] bg-[#7C8BBF]  hover:opacity-100  rounded-l-lg border overflow-visible  float-right '>
+   
+        <p className=' hover:bg-[#7239e526] ml-2 mr-2  cursor-pointer border-b-2 text-white'>Añadir a playlist</p>
+        <p className=' hover:bg-[#7239e526] ml-2 mr-2  cursor-pointer text-white'>Ir al artista</p>
+   
+    </div>
+    </div> 
+  )
+}
+
+export default DetailModal
