0e7973c321ab3ee6706009cadd67aeff56e8c5e4
Alvaro Sanchez <alvarosanro89@gmail.com>
2023-05-12 19:51:19 +0200

    add recent and swiper

--- src/component/Artist/Artist.jsx
+++ src/component/Artist/Artist.jsx
@@ -1,4 +1,10 @@
 import React, { useEffect, useState } from 'react'
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+// import "swiper/css/autoplay";
+import '../Swiper/Swiper.css'
+import { Autoplay, Navigation } from "swiper";
 
 const Artist = () => {
     const MusicUrl = "http://localhost:3004/artists"
@@ -14,21 +20,75 @@ const Artist = () => {
     }, [MusicUrl])
 
 
+    const handleReachEnd = (swiper) => {
+        const lastIndex = dataSong.length - 1;
+        const currentLastIndex = swiper.realIndex;
+        if (currentLastIndex === lastIndex) {
+          swiper.slideTo(0, 0, false);
+        }
+      };
+
   return (
+
+    <Swiper 
+        slidesPerView={4} 
+        autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+        }}
+        navigation={true} 
+        modules={[ Autoplay, Navigation]}
+        onReachEnd={handleReachEnd}
+        className="mySwiper"
+        breakpoints={{
+            "@0.00": {
+              slidesPerView: 1,
+              spaceBetween: 5,
+            },
+            "@0.40": {
+                slidesPerView: 2,
+                spaceBetween: 5,
+              },
+            "@0.75": {
+              slidesPerView: 3,
+              spaceBetween: 5,
+            },
+            "@1.00": {
+              slidesPerView: 4,
+              spaceBetween: 5,
+            },
+            "@1.25":{
+                slidesPerView: 4,
+                spaceBetween: 5,
+            },
+            "@1.50": {
+              slidesPerView: 4,
+              spaceBetween: 5,
+            },
+            "@2.00": {
+                slidesPerView: 5,
+                spaceBetween: 5,
+              },
+              "@2.50": {
+                slidesPerView: 6,
+                spaceBetween: 5,
+              },
+          }}
+        >
+
     <div className=' flex overflow-x-scroll scrollbar-hide w-full h-full whitespace-no-wrap  mt-5 lg:mt-10 '>
     {
-        dataSong.map((song) => {
-            return (
-                <div key={song.id}>
-                   <div className='mb-6  min-w-[12rem]  xl:min-w-[15rem] '>
-                        <img className='w-[90%] mx-auto rounded h-[12rem]' src={song.photoUrl} alt="first artist" />
-                        <p className='hidden lg:flex lg:text-[1.3rem] lg:mt-2 xl:text-[1.6rem] 2xl:text-[1rem] justify-center  font-semibold'>{song.name}</p>
+        dataSong.map((song) => (
+                <SwiperSlide key={song.id}>
+                    <div className='mb-6  min-w-[12rem]  xl:min-w-[15rem] '>
+                            <img className='w-[90%] mx-auto rounded h-[12rem]' src={song.photoUrl} alt="first artist" />
+                            <p className='hidden lg:flex lg:text-[1.3rem] lg:mt-2 xl:text-[1.6rem] 2xl:text-[1rem] justify-center  font-semibold'>{song.name}</p>
                     </div>
-                </div>
-                )
-        })
+                </SwiperSlide>
+        ))
     }
     </div>
+    </Swiper>
   )
 }
 
--- src/context/song/SongProvider.jsx
+++ src/context/song/SongProvider.jsx
@@ -1,97 +1,123 @@
-import React, { useState, useEffect, useContext } from 'react'
+import React, { useState, useEffect, useContext } from "react";
 
-import SongContext from './SongContext'
-import UserContext from '../UserContext'
+import SongContext from "./SongContext";
+import UserContext from "../UserContext";
 
-import { useAuth0 } from '@auth0/auth0-react'
-import { toast } from 'react-hot-toast'
-
-
+import { useAuth0 } from "@auth0/auth0-react";
+import { toast } from "react-hot-toast";
 
 const SongProvider = ({ children }) => {
+  const [dataSong, setDataSong] = useState([]);
 
-  const [dataSong, setDataSong] = useState([])
+  const [allPlaylistSong, setAllPlaylistSong] = useState([]);
+  const [onePlayListSong, setOnePlayListSong] = useState({});
+  const [recentSong, setRecentSong] = useState([]);
 
-  const [allPlaylistSong, setAllPlaylistSong] = useState([])
-  const [onePlayListSong, setOnePlayListSong] = useState({})
-  const [recentSong, setRecentSong] = useState({})
+  const { userLogged, dataUsers } = useContext(UserContext);
 
-  const { userLogged, dataUsers } = useContext(UserContext)
-
-  const { getIdTokenClaims } = useAuth0()
+  const { getIdTokenClaims } = useAuth0();
 
   const handleLikes = async (liked) => {
-
-    const token = await getIdTokenClaims()
-    const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/tracks/addToLike${userLogged && userLogged._id}`, {
-      method: "PUT",
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token ? token.__raw : "Not Auth"}`,
-      },
-      body: JSON.stringify({ liked })
-    })
-    const data = await response.json()
-    console.log(data)
+    const token = await getIdTokenClaims();
+    const response = await fetch(
+      `${process.env.REACT_APP_SERVER_URL}/tracks/addToLike${
+        userLogged && userLogged._id
+      }`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token ? token.__raw : "Not Auth"}`,
+        },
+        body: JSON.stringify({ liked }),
+      }
+    );
+    const data = await response.json();
+    console.log(data);
     try {
-
       const updateFilter = dataSong.filter((update) => {
-        return update._id !== data.updateLike._id
-      })
-      const dataAllSongs = [...updateFilter, data.updateLike].sort((a, b) => a._id.localeCompare(b._id))
-      setDataSong([...dataAllSongs])
-
+        return update._id !== data.updateLike._id;
+      });
+      const dataAllSongs = [...updateFilter, data.updateLike].sort((a, b) =>
+        a._id.localeCompare(b._id)
+      );
+      setDataSong([...dataAllSongs]);
     } catch (error) {
-      console.log(error)
+      console.log(error);
       if (data.error === "InvalidTokenError: Invalid Compact JWS") {
-        toast.error("Tienes que iniciar sesión para poder añadir me gusta")
+        toast.error("Tienes que iniciar sesión para poder añadir me gusta");
       }
     }
-  }
+  };
 
   const handleOpenSong = (song) => {
-    setOnePlayListSong(song)
-  }
-
-
-
+    setOnePlayListSong(song);
+    const dateAt = Date.now();
+    song.dateAt = dateAt;
+    console.log(song);
+    const addSongRecent = async () => {
+      const token = await getIdTokenClaims();
+      const response = await fetch(
+        `${process.env.REACT_APP_SERVER_URL}/recents/${userLogged._id}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token ? token.__raw : "Not Auth"}`,
+          },
+          body: JSON.stringify({ song }),
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+      try {
+      } catch (error) {
+        console.log(error);
+        if (data.error === "InvalidTokenError: Invalid Compact JWS") {
+          toast.error("Tienes que iniciar sesión para poder añadir me gusta");
+        }
+      }
+    };
+    addSongRecent();
+  };
 
   useEffect(() => {
     const musicTracks = async () => {
-      const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/tracks`);
+      const response = await fetch(
+        `${process.env.REACT_APP_SERVER_URL}/tracks`
+      );
       const data = await response.json();
-      setDataSong(data.allSong)
-    }
-    musicTracks()
-
-  }, [setDataSong])
-
-
+      setDataSong(data.allSong);
+    };
+    musicTracks();
+  }, [setDataSong]);
 
   const likesByUser = (song) => {
     if (userLogged) {
-      console.log(song)
-      return song.likedBy?.includes(userLogged._id) 
+      console.log(song);
+      return song.likedBy?.includes(userLogged._id);
     }
   };
- 
-
-
 
   return (
-    <SongContext.Provider value={{
-      dataSong, setDataSong,
-      allPlaylistSong, setAllPlaylistSong,
-      onePlayListSong, setOnePlayListSong,
-      recentSong, setRecentSong,
-      handleLikes: handleLikes,
-      likesByUser: likesByUser,
-      handleOpenSong: handleOpenSong,
-      
-    }}>
+    <SongContext.Provider
+      value={{
+        dataSong,
+        setDataSong,
+        allPlaylistSong,
+        setAllPlaylistSong,
+        onePlayListSong,
+        setOnePlayListSong,
+        recentSong,
+        setRecentSong,
+        handleLikes: handleLikes,
+        likesByUser: likesByUser,
+        handleOpenSong: handleOpenSong,
+      }}
+    >
       {children}
     </SongContext.Provider>
-  )
-}
+  );
+};
 
-export default SongProvider
+export default SongProvider;
