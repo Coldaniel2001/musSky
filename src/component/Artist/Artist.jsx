6ddd71ef001f7279732c346fd6d41147de4517a1
Alvaro Sanchez <alvarosanro89@gmail.com>
2023-05-13 12:36:40 +0200

    add swiper

--- src/component/Artist/Artist.jsx
+++ src/component/Artist/Artist.jsx
@@ -2,12 +2,12 @@ import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 
 import UserContext from '../../context/UserContext'
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import "swiper/css";
-// import "swiper/css/navigation";
-// // import "swiper/css/autoplay";
-// import '../Swiper/Swiper.css'
-// import { Autoplay, Navigation } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+// import "swiper/css/autoplay";
+import '../Swiper/Swiper.css'
+import { Autoplay, Navigation } from "swiper";
 
 const Artist = () => {
 
@@ -17,26 +17,81 @@ const Artist = () => {
         return artist.rol === "artist"
     })
 
-
-
+    const handleReachEnd = (swiper) => {
+        const lastIndex = filterArtists.length - 1;
+        const currentLastIndex = swiper.realIndex;
+        if (currentLastIndex === lastIndex) {
+          swiper.slideTo(0, 0, false);
+        }
+      };
 
     return (
+
+        <Swiper 
+        slidesPerView={4} 
+        autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+        }}
+        navigation={true} 
+        modules={[ Autoplay, Navigation]}
+        onReachEnd={handleReachEnd}
+        className="mySwiper"
+        breakpoints={{
+            "@0.00": {
+              slidesPerView: 1,
+              spaceBetween: 5,
+            },
+            "@0.40": {
+                slidesPerView: 2,
+                spaceBetween: 5,
+              },
+            "@0.75": {
+              slidesPerView: 3,
+              spaceBetween: 5,
+            },
+            "@1.00": {
+              slidesPerView: 4,
+              spaceBetween: 5,
+            },
+            "@1.25":{
+                slidesPerView: 4,
+                spaceBetween: 5,
+            },
+            "@1.50": {
+              slidesPerView: 4,
+              spaceBetween: 5,
+            },
+            "@2.00": {
+                slidesPerView: 5,
+                spaceBetween: 5,
+              },
+              "@2.50": {
+                slidesPerView: 6,
+                spaceBetween: 5,
+              },
+          }}
+        >
+
         <div className=' flex overflow-x-scroll scrollbar-hide w-full h-full whitespace-no-wrap  mt-5 lg:mt-10 '>
             {
                 filterArtists.map((artist) => {
                     return (
+                        <SwiperSlide>
                         <Link to={`/artist/${artist._id}`} >
                             <div key={artist._id} >
-                            <div className='mb-6  min-w-[12rem]  xl:min-w-[15rem] '>
+                            <div className='mb-6  min-w-[12rem]  xl:min-w-[15rem]'>
                                 <img className='w-[90%] mx-auto rounded h-[12rem]' src={artist.picture} alt="first artist" />
                                 <p className='hidden lg:flex lg:text-[1.3rem] lg:mt-2 xl:text-[1.6rem] 2xl:text-[1rem] justify-center  font-semibold'>{artist.name}</p>
                             </div>
                         </div>
                         </Link>
+                        </SwiperSlide>
                     )
                 })
             }
         </div>
+        </Swiper>
     )
 }
 
