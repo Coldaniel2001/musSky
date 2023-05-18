32dc0189fa7adf8e2300c624ea47288a77e8c7dc
Alvaro Sanchez <alvarosanro89@gmail.com>
2023-05-18 17:20:12 +0200

    clean details

--- src/component/Toggle/Toggle.jsx
+++ src/component/Toggle/Toggle.jsx
@@ -5,7 +5,7 @@ const Toggle = () => {
   return (
     <div className='mt-3.5'>
         <label className='relative inline-flex items-center cursor-pointer'>
-        <input type="checkbox" value="" className='sr-only peer' />
+        <input type="checkbox" value="" className='sr-only peer' checked={true}/>
         <div className="w-11 h-6 bg-detailsGray focus:outline-none rounded-full peer-checked:after:translate-x-full checked:after:border-white after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-mainPurple after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-mainPurple"></div>
         </label>
     </div>
