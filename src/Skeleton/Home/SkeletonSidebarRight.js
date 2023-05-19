a523b4d2291e86c5cac041be21ef53e992372354
Giorgi Meshveliani <xgorgaslax@gmail.com>
2023-05-19 09:41:36 +0200

    .

--- src/Skeleton/Home/SkeletonSidebarRight.js
+++ src/Skeleton/Home/SkeletonSidebarRight.js
@@ -24,25 +24,3 @@ export default SkeletonSidebarRight
 
 
 
-
-
-// import React from "react"
-// import ContentLoader from "react-content-loader"
-
-// const SkeletonSidebarRight = (props) => (
-//   <ContentLoader
-//     rtl
-//     speed={2}
-//     width={100}
-//     height={800}
-//     viewBox="0 0 280 800"
-//     backgroundColor="#7239e537"
-//     foregroundColor="#7239e537"
-//     {...props}
-//   >
-//     <rect x="4" y="16" rx="10" ry="10" width="368" height="82" />
-//     <rect x="5" y="134" rx="0" ry="10" width="369" height="796" />
-//   </ContentLoader>
-// )
-
-// export default SkeletonSidebarRight
