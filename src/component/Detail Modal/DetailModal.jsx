5301d63f272e70c85f7b04836dc2cdee2c9d7c09
Giorgi Meshveliani <xgorgaslax@gmail.com>
2023-05-19 08:44:07 +0200

    .

--- src/component/Detail Modal/DetailModal.jsx
+++ src/component/Detail Modal/DetailModal.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "../App.css"
-import { Link } from 'react-router-dom'
+
 
 
 function DetailModal({handleModal}) {
@@ -12,9 +12,9 @@ function DetailModal({handleModal}) {
     <div className='w-[11rem] h-[4rem] bg-[#7C8BBF]  hover:opacity-100  rounded border overflow-visible  float-right '>
    
         <p onClick={handleModal} className=' hover:bg-[#7239e526] ml-2 mr-2  cursor-pointer border-b-2 text-white'>Añadir a playlist</p>
-        {/* <Link to={`/artist/${artist.id}`}>
+       
           <p  className=' hover:bg-[#7239e526] ml-2 mr-2  cursor-pointer text-white'>Ir al artista</p>
-        </Link> */}
+        
    
     </div>
     </div> 
